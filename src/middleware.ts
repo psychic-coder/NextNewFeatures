2484bbeb2a184995d366965a98a1332f0322526c
psychic-coder <rohitganguly450@gmail.com>
2024-07-29 12:57:10 +0530

    made the sign-up page

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,27 +9,26 @@ import { getToken } from "next-auth/jwt"
 // This function can be marked `async` if using `await` inside
 // This is a middleware setup
 export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
 
-    const token =await getToken({req:request})
-    const url=request.nextUrl;
+
     //if we have the token and the url we're going towards is signin then we'll be redirected to dashboard as we're already signedin
-    if(token && (
-        url.pathname.startsWith('/sign-in')||
-        url.pathname.startsWith('/sign-up')||
-        url.pathname.startsWith('/verify')||
-        url.pathname.startsWith('/')
-    )){
-        return NextResponse.redirect(new URL('/dashboard', request.url))
+    if (
+      token &&
+      (url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/verify') ||
+        url.pathname === '/')
+    ) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
     }
-
-
-  return NextResponse.redirect(new URL('/home', request.url))
-}
- 
 
 //the below are the paths where we want out middleware to run
 // "/dashboard/:path*"--->this signifies all the paths of the dashboard
-export const config = {
-  matcher: ['/sign-in','/sign-up','/','/dashboard/:path*','/verify/:path*']
+if (!token && url.pathname.startsWith('/dashboard')) {
+  return NextResponse.redirect(new URL('/sign-in', request.url));
 }
 
+return NextResponse.next();
+}
